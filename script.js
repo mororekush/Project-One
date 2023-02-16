246ded3f2f50cd10e6363982204a43acf58167f8
Morore Kushimo <mororekush@gmail.com>
2023-02-16 21:34:23 +0100

    github link icnlusion

--- script.js
+++
@@ -1,6 +0,0 @@
-$("img").each( function( ) {
-    var src = $(this).attr("src");
-    if( /\.gif$/i.test(src)) {
-        $(this).attr( "src", src.replace( /\.gif$/, ".gif?rnd=" + Math.floor(Math.random() * 100) + 1));
-    }
-});
